8a93129661858251cc4ab8d29f094cf71a0cbc37
LucasGsalman <salmanlucas17@outlook.com>
2024-06-19 18:26:53 -0300

    realizando testes de layout

--- src/site-cliente/login-cliente.component.ts
+++ src/site-cliente/login-cliente.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-admin',
@@ -7,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginClienteComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+  activeContent: string = 'amatra';
+  currentDate: string = '';
+  currentYear: string = '';
+  currentTime: string = '';
 
-  ngOnInit(): void {
+  showContent(content: string) {
+    this.activeContent = content;
   }
 
+  ngOnInit(): void {
+    this.updateCurrentDateTime();
+    // Atualiza a cada segundo
+    setInterval(() => {
+      this.updateCurrentDateTime();
+    }, 1000);
+    this.activeContent = 'amatra';
+  }
 
+  private updateCurrentDateTime(): void {
+    const currentDate = new Date();
+    const day = currentDate.getDate().toString().padStart(2, '0');
+    const month = (currentDate.getMonth() + 1).toString().padStart(2, '0'); // Month is zero-based
+    const year = currentDate.getFullYear();
+    const hours = currentDate.getHours().toString().padStart(2, '0');
+    const minutes = currentDate.getMinutes().toString().padStart(2, '0');
+    const seconds = currentDate.getSeconds().toString().padStart(2, '0');
+
+    this.currentDate = `${day}/${month}`;
+    this.currentYear = `${year}`;
+    this.currentTime = `${hours}:${minutes}:${seconds}`;
+  }
 }
